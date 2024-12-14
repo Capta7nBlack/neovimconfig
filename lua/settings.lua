15e6cfadf4b789a0ad3b31b73a1afbf348b66104
Capta7nBlack <sayat.mushkin@nu.edu.kz>
2024-12-14 23:07:55 +0600

    Neovim Config Version Control

---
+++ lua/settings.lua
@@ -0,0 +1,15 @@
+-- opt as a variable for simplicity and readibility
+local opt = vim.opt
+
+-- sets the line counter
+opt.number = true
+
+-- sets the current directory to neovim, instead of terminal 
+opt.autochdir = true
+
+-- swapfile error disregardment 
+opt.swapfile = false
+
+
+
+
