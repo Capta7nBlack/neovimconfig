c16797c20c3e1d37f6ba6955fcdfb871734be279
Capta7nBlack <sayat.mushkin@nu.edu.kz>
2024-12-16 03:56:36 +0600

    Changed buffer navigation to <leader>hjlk

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -4,6 +4,36 @@ keymap = vim.keymap
 g.mapleader = " "
 g.maplocalleader = " "
 
+-- Exlorer Jump Instructions
+---- Explorer Opening Shortcut
 keymap.set('n', '<leader>ee', ':30vs.<CR>', { noremap = true, silent = true })
+
+-------- Explorer to Config Folder
 keymap.set('n', '<leader>ec', ':EditConfig<CR>', { noremap = true, silent = true })
+
+-------- Explorer to disk D
 keymap.set('n', '<leader>ed', ':Cdd<CR>', { noremap = true, silent = true })
+
+
+-- Quit and Save Shortcuts
+---- Quit Regardless
+keymap.set('n', '<leader>qq' , ':q!<CR>' , { noremap = true, silent = true})
+
+---- Write With no Quit
+keymap.set('n', '<leader>ww' , ':w<CR>' , { noremap = true, silent = true})
+
+-------- Write and Quite
+keymap.set('n', '<leader>wq' , ':wq<CR>' , { noremap = true, silent = true})
+
+-------- Quit All Regardless
+keymap.set('n', '<leader>Q' , ':qa!<CR>' , { noremap = true, silent = true})
+
+
+-- Buffer Management Shortcuts
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+
+
+
